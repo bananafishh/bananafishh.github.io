992558e876c93103d53f2eb9088a1e86ba86f417
bananafishh <z.dyshekova@gmail.com>
2017-05-09 14:09:30 +0300

    j

--- js/main.js
+++ js/main.js
@@ -98,7 +98,7 @@ var RegionalOfficesMap = Backbone.View.extend({
                 iconLayout: 'default#image',
                 iconImageHref: '/assets/images/icon-balloon--white.png',
                 iconImageSize: [26, 33],
-                iconImageOffset: [-20, -17]
+                iconImageOffset: [-17, -4]
             });
 
 
