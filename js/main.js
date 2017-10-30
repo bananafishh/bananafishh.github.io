be813ec27f52f7c38a2f86fb2396c5e2e1d8e6b4
bananafishh <z.dyshekova@gmail.com>
2017-10-31 01:45:54 +0300

    f

--- js/main.js
+++ js/main.js
@@ -173,11 +173,14 @@ App.Control.install({
     name: 'FancyMedia',
     initialize: function () {
         var self = this;
+		var fitToView = this.$el.data('fullsize') ? false : true;
 		this.$el.fancybox({
 			wrapCSS: 'fancy-media',
 			margin: ($(window).width() > 937) ? 20 : 5,
-			fitToView: false,
+			fitToView: fitToView,
 			padding: 15,
+			autoResize: true,
+			maxWidth: '100%',
 			helpers : {
 				overlay : {
 					css : {
